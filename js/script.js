6d51b6c312e1d7c9374e4a5b4841dda1ff1ad791
einarstensson <einar.stensson@gmail.com>
2016-03-15 12:12:09 -0400

    Done.

--- js/script.js
+++ js/script.js
@@ -2,8 +2,17 @@
 
 
   //try Fart Scroll here
+  fartscroll();
 
   //try fool.js here
-
+  $.fool('vanishingElements');
+//  $.fool('questionTime');
+//  $.fool('upsideDown');
+//  $.fool('h4xx0r');
+//  $.fool('wonky');
+//  $.fool('flash');
+//  $.fool('crashAndBurn');
+//  $.fool('shutter');
+//  $.fool('unclickable');
 
 });
